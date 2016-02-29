238fec50e29a2a22b7dbc50b73ea8c473f25ad12
alexegri <egrialexandru@gmail.com>
2016-02-29 21:29:16 +0200

    gandit in plus la clase si metode+variabile ajuns la stadiul de varza

---
+++ java-2016/week-3/catalogLibrarie/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class carti;
+ /* userul va putea sa:
+ 						aleaga operatiunea ce o aplica pe carti (adauge, scoata, listeze)
+							daca e carte noua sa ii aleaga tipul
+								adauge numarul de pagini
+								adauge numele cartii
+							scoata o carte din lista/array/vector/cum s-o fi numind structura de date care le tine :)
+							listeze toate cartile din lista
+							
+							
+clase necesare 
+carti
+	romane
+		tipRoman - String
+	albume
+		calitateHartie - String
+caracteristici comune
+numeCarte
+nrPagini
+
+metode din clase
+adaugaCarte
+scoateCarte
+listeazaCarti
+ */
+Scanner in = new Scanner(System.in);
+
+
+//aici clar nu e ok ceva
+//am ajuns la concluzia ca imi mai tb un array pe baza caruia sa pot sa imi stochez cartile :)) sa pot apoi sa le si listez de undeva numarul de pagini si numele
+private int nrPagini = nrPaginiCarteNoua();
+private string numeCarte = numeCarteNoua ();
+
+
+//confuzie maxima deocamdata :)
+public void adaugaCarte(){
+	System.out.println ("introduceti numarul de pagini al romanului");
+	private int nrPaginiCarteNoua = in.nextInt();
+	System.out.println("Introduceti numele romanului");
+	private string numeCarteNoua = in.nextString ();
+}
+public void scoateCarte ();
+public void listeazaCarti ();
+
+
+carti romane = new carti();
+
+
+carti albume = new carti();
