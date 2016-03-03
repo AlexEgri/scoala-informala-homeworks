0b84ef81c1f85c73b7cdca4ef35fb87a03217b9a
alexegri <egrialexandru@gmail.com>
2016-03-04 01:33:45 +0200

    am mai avansat putin, am reusit sa am facute clasa de carti si clasele ce o mostenesc am reusit sa imi fac un switch pentru a reactiona la inputul userului dar inca nu e verificat daca face ce tb compileaza Library.java dar inca nu e legata in vreun fel de restul claselor am nevoie masiva de ajutor pentru chestii basic :(

--- java-2016/week-3/catalogLibrarie/src/Library.java
+++ java-2016/week-3/catalogLibrarie/src/Library.java
@@ -1,35 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Library {
 
-		private ArrayList() inventoryBooks = new ArrayList();
+		private ArrayList inventoryBooks = new ArrayList();
 		private int idBooks = 0;
 	
 		public void userChoice (){
-			System.out.println("what operation do you want to perform: ");
+			System.out.println("what operation do you want to perform? choose 1) to add book; 2) to remove a book or 3) to list the books ");
 			Scanner choice = new Scanner(System.in);
-			int chosen = choice.nextInt();
-			
-			Switch (chosen){
-				case 1: chosen = 1;
+			int inputChosen = choice.nextInt();
+			int chosen = 1;
+			switch(chosen){
+				case 1: 
 				addBook();
 				break;
-				case 2: chosen = 2;
+				case 2: 
 				removeBook();
 				break;
-				case 3: chosen = 3;
+				case 3: 
 				listBook();
 				break;
-				case 4: chosen != 1 || chosen != 2 || chosen != 3;
+				default: 				
 				System.out.println("Invalid Choice!!");
 				break;
-				default: break;				
 			}
-			
-			
 		}
-		
-		
-	public static void main(String[] args) {
+		public void addBook(){
+			System.out.println("adaugam carte:");
+		}
+		public void removeBook(){
+			System.out.println("scoatem carte:");
+		}	
+		public void listBook(){
+			System.out.println("listam carte:");
+		}
+		public static void main(String[] args) {
 		
 	}
 	
